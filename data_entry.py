77dd8877feb7230ae2a8d05554331dd78826af8d
Shaunak999 <barcawithin@gmail.com>
2024-07-20 22:18:40 +0530

    changes made

--- data_entry.py
+++ data_entry.py
@@ -1,16 +1,39 @@
-import pandas as pd 
-import csv
 from datetime import datetime
+date_format= "%d-%m-%y"
+CATEGORIES = {
+    "I":"income",
+    "E":"expense"
+}
 
-class CSV:
-    csv_file = 'financial_data.csv'
+def get_date(prompt,allow_default=False):
+    date_str = input(prompt)
+    if allow_default and not date_str:
+        return datetime.today().strftime(date_format)
     
-    @classmethod
-    def initialize_csv(cls):
-        try:
-            pd.read_csv(cls.csv_file)
-        except FileNotFoundError:
-            df = pd.DataFrame(columns=["date", "amount", "category", "description"])
-            df.to_csv(cls.csv_file, index=False)
+    try:
+        valid_date= datetime.strptime(date_str,date_format)
+        return valid_date.strftime(date_format)
+    except ValueError:
+        print("invalid date format, please enter the date in .strftime(d-m-y)")
+        return get_date(prompt,allow_default)
 
-CSV.initialize_csv()
+def get_amount():
+    try:
+        amount = float(input("Enter the amount: "))
+        if amount <= 0:
+            raise ValueError("Amount must be a non-negative value!")
+        return amount
+    except ValueError as e:
+        print(e)
+        return get_amount()
+
+def get_category():
+    category = input("Enter the category ('I' for income or 'E' for expense): ").upper()
+    if category in CATEGORIES:
+        return CATEGORIES[category]
+    
+    print("Invalid category")
+
+
+def get_description():
+    return input("Enter the description(optional): ")
